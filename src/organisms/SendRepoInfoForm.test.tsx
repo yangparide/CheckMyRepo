29975daab55b7f47e2e549119db5e6b8349e3ba9
Paride Rosati <paride@MacBook-Air-di-Paride.local>
2023-07-21 12:15:39 +0200

    add test

--- src/organisms/SendRepoInfoForm.test.tsx
+++ src/organisms/SendRepoInfoForm.test.tsx
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import SendRepoInfoForm from "./SendRepoInfoForm";
+import SendRepoInfoForm, { checkFormValueErrors } from "./SendRepoInfoForm";
 
 test("render SendRepoInfoForm start button", () => {
   render(<SendRepoInfoForm />);
   const startButton = screen.getByText(/Procediamo/i);
   expect(startButton).toBeInTheDocument();
+});
+
+test("check if email value is not accepted", () => {
+  expect(checkFormValueErrors("test@test.com")).not.toBe(false);
 });
